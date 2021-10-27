5c954c223404d9e257a87aa40ea32bec45e960b1
UmaKamsala <kamsalauma@gmail.com>
2021-10-27 16:32:39 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Line_Comparision_Comp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparision Computation Program");
+        }
+    }
+}
